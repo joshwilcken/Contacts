dbf61e1aa8a6049b7af2b5222332dc2bb4ba76cf
Josh Wilcken <Joshua.Wilcken@audatex.com>
2020-03-24 09:38:32 -0600

    Final Commit

--- ClientApp/src/components/WarningModal.js
+++ ClientApp/src/components/WarningModal.js
@@ -3,7 +3,7 @@ import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
 export const WarningModal = (props) => (
     <Modal open={props.showWarning} basic size='small'>
-        <Header icon='archive' content='Please fill out all inputs before submitting.' />
+        <Header icon='archive' content='Please fill out all fields before submitting.' />
         <Modal.Actions>
             <Button basic color='red' inverted onClick={props.onOk}>
                 <Icon name='remove' /> Ok
